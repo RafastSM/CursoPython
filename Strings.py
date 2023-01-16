cf24ffe402d74f372efd96aada965ed3fcefe379
RafastSM <shurikenstudiosrst@gmail.com>
2023-01-16 13:31:38 -0600

    Se agrego comentario y final

--- Strings.py
+++ Strings.py
@@ -1,2 +1,3 @@
-
+"""Este codigo pregunta tu animal y tu color favoritos y te devuelve el nombre de una cerveza"""
 print("Tu cerveza debería llamarse:\n"+'"'+input("¿Cual es tu animal favorito?")+' '+input("Cual es tu color favorito?")+'"')
+print("¿Te gusta?")
